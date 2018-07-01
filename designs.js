b649aec6f2d86b57494c2576238d108c05235271
chubaezeks <ezekschuba@gmail.com>
2018-07-01 06:55:45 +0100

    Complete code for final project

--- designs.js
+++ designs.js
@@ -1,19 +1,43 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-var gridHeight = $('#inputHeight').val();
-var gridWidth = $('#inputWidth').val();
 
-for (x=0; x <= gridHeight - 1; x++) {
-  theGrid.append("<tr>");
-for (y=0; y <= gridWidth; y++) {
-  theGrid.append("<td>");
+	// Create necessary variables for components
+
+	var canvas, gridHeight, gridWidth;
+
+	canvas = $('#pixel_canvas');
+	gridHeight = $('#inputHeight').val();
+	gridWidth = $('#inputWidth').val();
+
+// Loop over each row
+	for (x = 0; x < gridHeight; x++) {
+	canvas.append('<tr>');
+	}
+
+  var rows;
+	rows = $('tr');
+
+	for (y = 0; y < gridWidth; y++) {
+	rows.append('<td>');
+	}
+
+  var cell;
+	cell = canvas.find('td');
+
+	// Change color when colorPicker is clicked
+	cell.click(function() {
+		// Select color
+		var color;
+		color = $("#colorPicker").val();
+		$(this).attr('bgcolor', color);
+	});
 }
-}
+
+// Call makeGrid() when submit button is clicked
+var submitQuery;
+
+submitQuery = $('input[type="submit"]')
+
+submitQuery.click(function(event) {
+  event.preventDefault();
+  makeGrid();
 });
-// Your code goes here!
-
-}
